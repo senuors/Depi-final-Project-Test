7246802371201a40a17b106c1864a28a11074c7c
Mhmd Qassem <senuors@gmail.com>
2025-08-22 12:20:57 +0300

    p1

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Git Hub edit 2 Home Page'),
     );
   }
 }
